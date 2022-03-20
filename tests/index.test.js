62edda3078eece1fe8a4b231efba9a9487ed9fd3
Mads Jon Nielsen <madsjon@gmail.com>
2022-03-20 20:14:30 +0100

    Test run_scripts.js a little mre

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,21 +1,63 @@
 const {getProjectDirFromRemote} = require("../src/project");
+const {runScripts} = require("../src/run_scripts");
+const cp = require("promisify-child-process");
+const fs = require("fs-extra");
+const chalk = require("chalk");
+
+let cwd, projectObj, readFileSpy, pathExistsSpy;
+beforeEach(() => {
+	cp.spawn = jest.fn();
+	console.log = jest.fn();
+
+	readFileSpy = jest.spyOn(fs, "readFile").mockImplementation(() => "---");
+	pathExistsSpy = jest.spyOn(fs, "pathExists").mockImplementation(() => true);
+
+	cwd = "/home/user/git-local-devops";
+	projectObj = {
+		default_branch: "main",
+		remote: "git@gitlab.com:firecow/example.git",
+		scripts: {
+			start: {
+				"firecow.dk": ["docker-compose", "up"],
+				"example.com": ["scp", "user@example.com", "sh", "-c", "service", "webserver", "start"],
+			},
+			down: {
+				"firecow.dk": ["docker-compose", "down"],
+				"example.com": ["scp", "user@example.com", "sh", "-c", "service", "webserver", "stop"],
+			},
+		},
+	};
+});
+afterEach(() => {
+	jest.clearAllMocks();
+});
+
 
 describe("Project dir from remote", () => {
 
 	test("valid ssh remote", () => {
-		const dir = getProjectDirFromRemote("/home/user/git-local-devops", "git@gitlab.com:firecow/example.git");
-		expect(dir).toEqual("/home/user/git-local-devops/firecow/example");
+		const dir = getProjectDirFromRemote(cwd, "git@gitlab.com:firecow/example.git");
+		expect(dir).toEqual(`${cwd}/firecow/example`);
 	});
 
 	test("valid ssh remote with cwd ending in slash", () => {
-		const dir = getProjectDirFromRemote("/home/user/git-local-devops/", "git@gitlab.com:firecow/example.git");
-		expect(dir).toEqual("/home/user/git-local-devops/firecow/example");
+		const dir = getProjectDirFromRemote(`${cwd}/`, "git@gitlab.com:firecow/example.git");
+		expect(dir).toEqual(`${cwd}/firecow/example`);
 	});
 
-	test("invvalid remote", () => {
+	test("invalid remote", () => {
 		expect(() => {
-			getProjectDirFromRemote("/home/user/git-local-devops/", "git@gitlab.coinvalidirecow/example.git");
+			getProjectDirFromRemote(cwd, "git@gitlab.coinvalidirecow/example.git");
 		}).toThrowError("git@gitlab.coinvalidirecow/example.git is not a valid project remote. Use git@gitlab.com:example/firecow.git syntax");
 	});
+
 });
 
+describe("Script execution", () => {
+
+	test("runScripts start firecow.dk", async () => {
+		await runScripts(cwd, projectObj, "start", "firecow.dk");
+		expect(console.log).toHaveBeenCalledWith(chalk`Executing {blue docker-compose up} in {cyan /home/user/git-local-devops/firecow/example}`);
+	});
+
+});
