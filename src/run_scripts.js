8251d69b11ccfd7b35a615fce5d2dd6a37a19e29
Mads Jon Nielsen <madsjon@gmail.com>
2022-03-22 16:16:48 +0100

    Remove useless res

--- src/run_scripts.js
+++ src/run_scripts.js
@@ -7,14 +7,13 @@ async function runScripts(cwd, projectObj, scriptToRun, domainToRun) {
 	const remote = projectObj["remote"];
 	const scriptsObj = projectObj["scripts"];
 	const dir = getProjectDirFromRemote(cwd, remote);
-	let err, res;
+	let err;
 
 	for (const [scriptName, domainsObj] of Object.entries(scriptsObj)) {
-		// noinspection JSCheckFunctionSignatures
 		for (const [domain, argv] of Object.entries(domainsObj)) {
 			if (scriptName !== scriptToRun || domain !== domainToRun) continue;
 			console.log(chalk`Executing {blue ${argv.join(" ")}} in {cyan ${dir}}`);
-			[err, res] = await to(cp.spawn(argv[0], argv.slice(1), {cwd: dir, env: process.env, encoding: "utf8"}));
+			[err] = await to(cp.spawn(argv[0], argv.slice(1), {cwd: dir, env: process.env, encoding: "utf8"}));
 			if (err) {
 				console.error(chalk`${scriptToRun} for ${domainToRun} failed, goto {cyan ${dir}} and run {blue ${argv.join(" ")}} manually`);
 			}
