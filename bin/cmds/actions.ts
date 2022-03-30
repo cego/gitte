2470e9d633a322e22a04ba763610a242903a3ff1
Mads Jon Nielsen <madsjon@gmail.com>
2022-03-30 13:56:21 +0200

    Add yargs.Argv positionals in bin/actions.ts and bin/run.ts

--- bin/cmds/actions.ts
+++ bin/cmds/actions.ts
@@ -1,6 +1,19 @@
 import { loadConfig } from "../../src/config_loader";
 import { fromConfig } from "../../src/actions";
+import { Argv } from "yargs";
 
+// noinspection JSUnusedGlobalSymbols
+export function builder(y: Argv) {
+	return y
+		.positional("action", {
+			required: false,
+			describe: "action to run for each project in config",
+		})
+		.positional("group", {
+			required: false,
+			describe: "group entry to run for specified action",
+		});
+}
 // noinspection JSUnusedGlobalSymbols
 export const command = "actions <action> <group>";
 // noinspection JSUnusedGlobalSymbols
--- bin/cmds/run.ts
+++ bin/cmds/run.ts
@@ -2,7 +2,20 @@ import { loadConfig } from "../../src/config_loader";
 import { startup } from "../../src/startup";
 import { fromConfig as gitOpsFromConfig } from "../../src/gitops";
 import { fromConfig as actionsFromConfig } from "../../src/actions";
+import { Argv } from "yargs";
 
+// noinspection JSUnusedGlobalSymbols
+export function builder(y: Argv) {
+	return y
+		.positional("action", {
+			required: false,
+			describe: "action to run for each project in config",
+		})
+		.positional("group", {
+			required: false,
+			describe: "group entry to run for specified action",
+		});
+}
 // noinspection JSUnusedGlobalSymbols
 export const command = "run <action> <group>";
 // noinspection JSUnusedGlobalSymbols
