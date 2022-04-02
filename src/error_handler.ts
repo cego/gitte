b03bc16e4c0968a230606af68f07d6b039991842
Mads Jon Nielsen <madsjon@gmail.com>
2022-04-02 15:05:56 +0200

    Improve child process errors handling and make sure startup hints are printed

--- src/error_handler.ts
+++ src/error_handler.ts
@@ -1,11 +1,16 @@
 import { AssertionError } from "assert";
 import chalk from "chalk";
 
-export function errorHandler(err: unknown) {
+export function errorHandler(err: any) {
 	if (err instanceof AssertionError) {
 		console.error(chalk`{red ${err.message}}`);
+	} else if (err.code) {
+		console.error(chalk`{red ${err.stderr.replace(/\n$/, "")}}`);
 	} else if (err instanceof Error) {
 		console.error(chalk`{red ${err.stack}}`);
+	}
+	if (err?.hint) {
+		console.log(err.hint);
 	}
 	process.exit(1);
 }
