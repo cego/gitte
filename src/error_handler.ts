41ecb61044b83d0771bd76de05f821bcfcbf1dfc
Lau Ernebjerg Josefsen <44977457+LauJosefsen@users.noreply.github.com>
2022-04-14 18:33:42 +0200

    Fail hard on multiple actions if one fails (#100)
    * Made errors be logged without stack trace more reliably and fail fast if any error happens
    
    * Made errors be logged without stack trace more reliably and fail fast if any error happens
    
    * Add throw test
    
    * Fixed test

--- src/error_handler.ts
+++ src/error_handler.ts
@@ -3,16 +3,22 @@ import chalk from "chalk";
 import { ErrorWithHint } from "./types/utils";
 
 export function errorHandler(err: any) {
+	let handled = false; // if the error is handled, we don't want to print stack trace
 	if (err instanceof AssertionError) {
 		console.error(chalk`{red ${err.message}}`);
-	} else if (err.exitCode && err.exitCode !== 0) {
+		handled = true;
+	}
+	if (err.exitCode && err.exitCode !== 0) {
 		if (err.stderr) console.error(chalk`{red ${err.stderr?.replace(/\n$/, "")}}`);
 		if (err.stdout) console.log(chalk`${err.stdout?.replace(/\n$/, "")}`);
+		handled = true;
 	}
-
 	if (err instanceof ErrorWithHint) {
 		console.log(err.hint);
-	} else {
+		handled = true;
+	}
+
+	if (!handled) {
 		console.error(chalk`{red ${err.stack}}`);
 	}
 	process.exit(1);
