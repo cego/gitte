8656a22910c65c58479db8f9144b69cf6a5229b1
Dennis Staal <dest@cego.dk>
2024-01-19 12:05:31 +0100

    Initial commit

--- bin/cmds/list.ts
+++ bin/cmds/list.ts
@@ -10,8 +10,32 @@ export const describe = "List all projects and their actions";
 export async function handler(argv: any) {
 	try {
 		const config = await loadConfig(argv.cwd as string, argv.needs, false);
+		let action_obj: Record<string, Record<string, string[]>> = {};
+
 		for (const [name, project] of Object.entries(config.projects)) {
-			console.log(chalk`{bold ${name}:} {cyan [${Object.keys(project.actions).join(", ")}]}`);
+			const groups = Object.keys(project.actions);
+
+			// instantiate
+			action_obj[name] = {};
+
+			groups.forEach(function (group) {
+				action_obj[name][group] = Object.keys(project.actions[group].groups);
+			});
+		}
+
+		for (const name in action_obj) {
+			let group_action = "";
+			let i = Object.keys(action_obj[name]).length;
+
+			for (const group in action_obj[name]) {
+				if (i <= 1) {
+					group_action += chalk`{cyan <${group}>} {magenta <${action_obj[name][group]}>}`;
+				} else {
+					group_action += chalk`{cyan <${group}>} {magenta <${action_obj[name][group]}>}, `;
+				}
+				--i;
+			}
+			console.log(chalk`{bold ${name}:} ${group_action}`);
 		}
 	} catch (e) {
 		errorHandler(e);
