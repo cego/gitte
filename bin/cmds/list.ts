615968692c86fe66d0cebaf2cc79c0d3a3c105ee
Dennis Staal <dest@cego.dk>
2024-01-19 14:59:44 +0100

    Use map function

--- bin/cmds/list.ts
+++ bin/cmds/list.ts
@@ -1,41 +1,25 @@
 import { loadConfig } from "../../src/config_loader";
 import chalk from "chalk";
 import { errorHandler } from "../../src/error_handler";
+import { ProjectAction } from "../../src/types/config";
 
 // noinspection JSUnusedGlobalSymbols
 export const command = "list";
 // noinspection JSUnusedGlobalSymbols
 export const describe = "List all projects and their actions";
+
+function actionToPrettyString(actions: [string, ProjectAction]) {
+		return chalk`{cyan <${actions[0]}>} {magenta <${Object.keys(actions[1].groups)}>}`;
+}
+
 // noinspection JSUnusedGlobalSymbols
 export async function handler(argv: any) {
 	try {
 		const config = await loadConfig(argv.cwd as string, argv.needs, false);
-		const action_obj: Record<string, Record<string, string[]>> = {};
+		// const action_obj: Record<string, Record<string, string[]>> = {};
 
 		for (const [name, project] of Object.entries(config.projects)) {
-			const groups = Object.keys(project.actions);
-
-			// instantiate
-			action_obj[name] = {};
-
-			groups.forEach(function (group) {
-				action_obj[name][group] = Object.keys(project.actions[group].groups);
-			});
-		}
-
-		for (const name in action_obj) {
-			let group_action = "";
-			let i = Object.keys(action_obj[name]).length;
-
-			for (const group in action_obj[name]) {
-				if (i <= 1) {
-					group_action += chalk`{cyan <${group}>} {magenta <${action_obj[name][group]}>}`;
-				} else {
-					group_action += chalk`{cyan <${group}>} {magenta <${action_obj[name][group]}>}, `;
-				}
-				--i;
-			}
-			console.log(chalk`{bold ${name}:} ${group_action}`);
+			console.log(chalk`{bold ${name}:} ${Object.entries(project.actions).map(actionToPrettyString)}`);
 		}
 	} catch (e) {
 		errorHandler(e);
