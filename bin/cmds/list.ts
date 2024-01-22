8b5ea2b7ce060f81ba77328df662bff3d7e53201
Staal <48656753+staal0@users.noreply.github.com>
2024-01-22 09:25:46 +0100

    Show groups in gitte list output (#193)
    * Initial commit
    
    * Changed action_obj to const
    
    * Use map function
    
    * Prettier optimized
    
    ---------
    
    Co-authored-by: Dennis Staal <dest@cego.dk>

--- bin/cmds/list.ts
+++ bin/cmds/list.ts
@@ -1,17 +1,25 @@
 import { loadConfig } from "../../src/config_loader";
 import chalk from "chalk";
 import { errorHandler } from "../../src/error_handler";
+import { ProjectAction } from "../../src/types/config";
 
 // noinspection JSUnusedGlobalSymbols
 export const command = "list";
 // noinspection JSUnusedGlobalSymbols
 export const describe = "List all projects and their actions";
+
+function actionToPrettyString(actions: [string, ProjectAction]) {
+	return chalk`{cyan <${actions[0]}>} {magenta <${Object.keys(actions[1].groups)}>}`;
+}
+
 // noinspection JSUnusedGlobalSymbols
 export async function handler(argv: any) {
 	try {
 		const config = await loadConfig(argv.cwd as string, argv.needs, false);
+		// const action_obj: Record<string, Record<string, string[]>> = {};
+
 		for (const [name, project] of Object.entries(config.projects)) {
-			console.log(chalk`{bold ${name}:} {cyan [${Object.keys(project.actions).join(", ")}]}`);
+			console.log(chalk`{bold ${name}:} ${Object.entries(project.actions).map(actionToPrettyString)}`);
 		}
 	} catch (e) {
 		errorHandler(e);
