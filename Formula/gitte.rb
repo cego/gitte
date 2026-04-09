b0fa828d55fea7df2cb165d87e58431477ef351b
CI <noc+gitte@cego.dk>
2026-04-09 11:51:22 +0000

    Update Homebrew formula for 2.0.0-rc.12

--- Formula/gitte.rb
+++ Formula/gitte.rb
@@ -1,31 +1,32 @@
 class Gitte < Formula
   desc "Developer environment orchestration tool"
   homepage "https://github.com/cego/gitte"
-  version "2.0.0-rc.11"
+  version "2.0.0-rc.12"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/cego/gitte/releases/download/2.0.0-rc.11/gitte-darwin-arm64.tar.gz"
-      sha256 "247a728960485e921dc031215aba4a704ec1e5d68f92b3e78e0259245b238e4a"
+      url "https://github.com/cego/gitte/releases/download/2.0.0-rc.12/gitte-darwin-arm64.tar.gz"
+      sha256 "e057f6716b774cf94b4b733251460dd24968656ddcc72e9be188164e7403a687"
     else
-      url "https://github.com/cego/gitte/releases/download/2.0.0-rc.11/gitte-darwin-amd64.tar.gz"
-      sha256 "011d3e69d32cf581a2f61a27a5e12acb0e6df90638364ba9ebfc1b441af0c128"
+      url "https://github.com/cego/gitte/releases/download/2.0.0-rc.12/gitte-darwin-amd64.tar.gz"
+      sha256 "b49dfe61f158c40d8fce9e52d219bf9319c3cc877e4d019a0c75f38d5ae510b6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/cego/gitte/releases/download/2.0.0-rc.11/gitte-linux-arm64.tar.gz"
-      sha256 "a401def4d36d92567f630502d04db51d82aaec2ccbc0955b360eff231d6c75cf"
+      url "https://github.com/cego/gitte/releases/download/2.0.0-rc.12/gitte-linux-arm64.tar.gz"
+      sha256 "a2e42ccc496cf21649ed02127290baa16c96b8511b1219a1ef59328c4d0a5923"
     else
-      url "https://github.com/cego/gitte/releases/download/2.0.0-rc.11/gitte-linux-amd64.tar.gz"
-      sha256 "76243d27f89936257304942684212b2c9c05f11290b4db66e86339cd2437cb04"
+      url "https://github.com/cego/gitte/releases/download/2.0.0-rc.12/gitte-linux-amd64.tar.gz"
+      sha256 "f7af3247b4e96ef62783907e0e0ff3344b1695c8880985c49fb183fddd8fd269"
     end
   end
 
   def install
     bin.install "gitte"
+    generate_completions_from_executable(bin/"gitte", "completion")
   end
 
   test do
