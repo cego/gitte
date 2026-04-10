d354baefeac8f286c3ccec2eaf66891d181992c2
Lau Josefsen <lejo@cego.dk>
2026-04-10 09:20:31 +0200

    Default version to dev so --version always works

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/cego/gitte/cmd"
 
 // version is set at build time via -ldflags "-X main.version=<tag>"
-var version string
+var version = "dev"
 
 func main() {
 	cmd.SetVersion(version)
